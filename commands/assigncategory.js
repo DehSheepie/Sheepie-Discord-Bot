295b8dbf7e820181759555834555d835a7736422
Sheepie <chrisparsonsm97@yahoo.co.uk>
2020-12-05 22:18:51 +0000

    Changed the assigncategory command so that we check if the args exist.

--- commands/assigncategory.js
+++ commands/assigncategory.js
@@ -6,6 +6,8 @@ module.exports = {
 	execute(message, args) {
     let rawdata = fs.readFileSync(path.resolve(__dirname, "data/games.json"));
     let games = JSON.parse(rawdata);
+		let rawdata2 = fs.readFileSync(path.resolve(__dirname, "data/categories.json"));
+		let categories = JSON.parse(rawdata2);
 
 		// Create args
 		var game_name = "";
@@ -15,50 +17,91 @@ module.exports = {
 		// Get args
     for (i = 0; i < args.length; i++)
     {
+			// Check if we are now stitching the category together.
+			// Additionally checks if we have the last word in category
 			if (category_bool == true & i >= args.length - 1)
 			{
 				category_name += args[i];
 			}
+			// Only checks if we are now stitching the category together.
 			else if (category_bool == true)
 			{
 				category_name += args[i] + " ";
 			}
+			// Checks whether we have the last word in the game name
 			else if (args[i][args[i].length - 1] == ":")
 			{
 				game_name += args[i].slice(0, -1);
 				category_bool = true;
 			}
+			// Otherwise we probably have word in the game name that isn't the last one
 			else
 			{
 				game_name += args[i] + " ";
 				console.log(`invalid arg: ${args[i]}`);
 			}
     }
+
+		var game_found = false;
+		var category_found = false;
+
 		console.log(`Game name:${game_name} | Category name:${category_name}`);
 		console.log(game_name & category_name);
+		// Check that the game and category names both aren't empty
 		if (!game_name == "" & !category_name == "")
 		{
+			// Check for game name in the games file
 			for (i = 0; i < games.length; i++)
 			{
 				if (games[i].name == game_name)
 				{
-					if (games[i].hasOwnProperty('categories'))
-					{
-						games[i].categories.push(category_name)
-					}
-					else
-					{
-						games[i].categories = [category_name];
-					}
-
-					fs.writeFileSync( "./commands/data/games.json", JSON.stringify(games));
-					message.channel.send("Game: " + game_name + " assigned to category: " + category_name);
+					game_found = true
 				}
+			}
+			// Check for category name in category file
+			for (i = 0; i < categories.length; i++)
+			{
+				if (categories[i].name == category_name)
+				{
+					category_found = true
+				}
+			}
+			// Check that the game and category was found
+			if (game_found & category_found)
+			{
+				// Get game and add category
+				let game_entry_index = games.findIndex(arg => arg.name == game_name);
+				console.log(games[game_entry_index]);
+				games[game_entry_index]['categories'].push(category_name);
+
+				// Get category and add game
+				let category_entry_index = categories.findIndex(arg => arg.name == category_name);
+				console.log(categories[category_entry_index]);
+				categories[category_entry_index]['games'].push(game_name);
+
+				// Write to game file
+				fs.writeFileSync( "./commands/data/games.json", JSON.stringify(games));
+				// Write to category file
+				fs.writeFileSync( "./commands/data/categories.json", JSON.stringify(categories));
+
+				message.channel.send(`[${game_name}] is now in the category [${category_name}]`);
+			}
+			else if (game_found)
+			{
+				message.channel.send(`Category: [${category_name}] not found.`);
+			}
+			else if (category_found)
+			{
+				message.channel.send(`Game: [${game_name}] not found.`);
+			}
+			else
+			{
+				message.channel.send(`Neither the game: [${game_name}] nor category: [${category_name}] was found.`);
 			}
 		}
 		else
 		{
-			message.channel.send("Game or Category invalid.");
+			message.channel.send("Game or Category empty.");
 		}
 	}
 };
