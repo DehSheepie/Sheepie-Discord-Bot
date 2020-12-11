11fb9aee326d820fcc7cab6e86ec969ebdee5cc2
Sheepie <chrisparsonsm97@yahoo.co.uk>
2020-12-11 23:41:10 +0000

    Added a file for commonly used functions.

---
+++ functions.js
@@ -0,0 +1,17 @@
+module.exports = {
+  ArgsToString : function(args)
+  {
+    return_value = "";
+    for (i = 0; i < args.length; i++)
+    {
+      if (i < args.length - 1)
+      {
+        return_value += args[i] + " ";
+      }
+      else
+      {
+        return_value += args[i];
+      }
+    }
+  }
+}
