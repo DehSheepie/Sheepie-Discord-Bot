1e1ae08ddb3397ae404e88205b889944719950a7
Sheepie <chrisparsonsm97@yahoo.co.uk>
2020-12-20 23:26:51 +0000

    Added a check in case the category already exists

--- commands/addcategory.js
+++ commands/addcategory.js
@@ -6,21 +6,30 @@ module.exports = {
 	description: 'Adds a category to the stored categories.',
 	execute(message, args) {
 // TODO: Add code to allow for an empty json file
+// TODO: Add a check in the case that the category already exists
     let categories = functions.getCategoriesData();
     var category_name = functions.argsToString(args);
 
     if (category_name != "")
     {
-      categories.push({name: category_name, games: []});
-      let cleaned = categories.map(item => {
-        return { name: item.name[0], games: []};
-      });
-      fs.writeFileSync("./commands/data/categories.json", JSON.stringify(categories));
-      message.channel.send("Category: " + category_name + " added.");
+			if (!functions.checkCategoryExists(category_name))
+			{
+				categories.push({name: category_name, games: []});
+	      let cleaned = categories.map(item => {
+	        return { name: item.name[0], games: []};
+	      });
+	      fs.writeFileSync("./commands/data/categories.json", JSON.stringify(categories));
+	      message.channel.send("Category: " + category_name + " added.");
+			}
+      else {
+      	{
+								message.channel.send(":no_entry_sign: Category already exists. :no_entry_sign:")
+				}
+      }
     }
 		else
 		{
-			message.channel.send("Please enter a category name.")
+			message.channel.send(":no_entry_sign: Please enter a category name.")
 		}
 	}
 };
