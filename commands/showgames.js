2f0a70a2f38d3e42dd649c2597000ffe8a455ba9
Sheepie <chrisparsonsm97@yahoo.co.uk>
2020-12-20 23:40:14 +0000

    added command for showing all games

---
+++ commands/showgames.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const functions = require('../functions.js');
+
+module.exports = {
+	name: 'showgames',
+	description: 'Shows all the games and games in them.',
+	execute(message, args) {
+			games = functions.getGamesData();
+
+			let output = "";
+			games.forEach((game, index) => {
+				output += `${index}: ${game.name}: ${functions.getCategories(game.name)}\n`;
+			});
+
+			message.channel.send(`\`\`\`${output}\`\`\``);
+	}
+};
