40bafff07ecbe2115d6a57342e6771e6def3d780
Sheepie <chrisparsonsm97@yahoo.co.uk>
2020-12-21 21:20:05 +0000

    added the new check function to my addgame command

--- commands/addgame.js
+++ commands/addgame.js
@@ -11,16 +11,23 @@ module.exports = {
 
     if (game_name != "")
     {
-      games.push({name: game_name});
-      let cleaned = games.map(item => {
-        return { name: item.name[0]};
-      });
-      fs.writeFileSync("./commands/data/games.json", JSON.stringify(games));
-      message.channel.send("Game: " + game_name + " added.");
+			if (!functions.checkGameExists(game_name))
+			{
+				games.push({name: game_name});
+	      let cleaned = games.map(item => {
+	        return { name: item.name[0]};
+	      });
+	      fs.writeFileSync("./commands/data/games.json", JSON.stringify(games));
+	      message.channel.send("Game: [" + game_name + "] added.");
+			}
+    	else
+			{
+				message.channel.send(":warning: Game already exists. :warning:");
+			}
     }
 		else
 		{
-			message.channel.send(":no_entry_sign: Please enter a game name. :no_entry_sign:")
+			message.channel.send(":no_entry_sign: Please enter a game name. :no_entry_sign:");
 		}
 	}
 };
