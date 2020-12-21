66d615f6ccf43bbca276ed9bd02be9727ca1de96
Sheepie <chrisparsonsm97@yahoo.co.uk>
2020-12-21 21:21:59 +0000

    added an error message in case the game doesn't exist

--- commands/removegame.js
+++ commands/removegame.js
@@ -11,22 +11,33 @@ module.exports = {
 
     if (game_name != "")
     {
+			// findIndex returns -1 on fail to find
 			let game_index = games.findIndex(arg => arg.name == game_name);
-			games.splice(game_index, 1);
 
-			// Creates a new list of categories that excludes the recently deleted game
-			let categories = functions.clearCategories(game_name);
+			if (!game_index === -1)
+			{
+				games.splice(game_index, 1);
 
-			// Write to category file
-			fs.writeFileSync( "./commands/data/categories.json", JSON.stringify(categories));
-			// Write to game file
-			fs.writeFileSync("./commands/data/games.json", JSON.stringify(games));
+				// Creates a new list of categories that excludes the recently deleted game
+				let categories = functions.clearCategories(game_name);
 
-			message.channel.send("Game deleted :wastebasket:");
+				// Write to category file
+				fs.writeFileSync( "./commands/data/categories.json", JSON.stringify(categories));
+				// Write to game file
+				fs.writeFileSync("./commands/data/games.json", JSON.stringify(games));
+
+				message.channel.send(`Game: [${game_name}] deleted :wastebasket:`);
+			}
+			else {
+				{
+					message.channel.send(":no_entry_sign: Game not found. :no_entry_sign:");
+				}
+			}
+
     }
 		else
 		{
-			message.channel.send(":warning: Please enter a game name. :warning:")
+			message.channel.send(":warning: Please enter a game name. :warning:");
 		}
 	}
 };
