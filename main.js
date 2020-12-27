f28233173d5ca20c6ad002de06df772d0df53b4a
Sheepie <chrisparsonsm97@yahoo.co.uk>
2020-12-27 05:00:19 +0000

    bot now expects the user to have a role

--- main.js
+++ main.js
@@ -24,16 +24,29 @@ client.on('message', message =>{
   const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
 
-  // If command doesn't exist, do nothing.
-  if (!client.commands.has(command)) return;
-
-  // Attempt to run command unless it is bugged.
-  try {
-    client.commands.get(command).execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply('there was an error trying to execute that command :(');
+  // If command doesn't exist show error.
+  if (!client.commands.has(command))
+  {
+    message.reply('Invalid command. :no_entry_sign:');
+    return;
   }
+  
+  // Check if member has the appropriate role for the bot
+  if (message.member.roles.cache.find(role => role.name === "sheepie-bot"))
+  {
+    // Attempt to run command unless it is bugged.
+    try {
+      client.commands.get(command).execute(message, args);
+    } catch (error) {
+      console.error(error);
+      message.reply('There was an error trying to execute that command :( :no_entry_sign:');
+    }
+  }
+  else
+  {
+    message.reply('You don\'t have the bot role. Sorry :( :warning:');
+  }
+
 });
 
 client.login(token);
