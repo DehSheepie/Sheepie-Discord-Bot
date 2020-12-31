e16445417c7d68e6014cbca67d5c4b5a52dc522b
Sheepie <chrisparsonsm97@yahoo.co.uk>
2020-12-31 19:44:13 +0000

    Fixed the description.

---
+++ commands/removeevent.js
@@ -0,0 +1,44 @@
+const functions = require('../functions.js');
+const fs = require('fs');
+
+module.exports = {
+  name: 'addevent',
+  description: "Adds a group for events to be added to",
+  execute(message, args) {
+
+    let text = functions.argsToString(args);
+    let values = text.split(':');
+
+    if (values.length === 5)
+    {
+      if (fs.existsSync(`./commands/data/events/${values[0]}.json`))
+      {
+
+        let group = functions.getGroupsData([values[0]]);
+
+        event =
+        {
+          name: values[1],
+          date: values[2],
+          time: values[3],
+          info: values[4],
+          upcoming: true
+        }
+
+        group.events.push(event);
+
+        fs.writeFileSync(`./commands/data/events/${values[0]}.json`, JSON.stringify(group));
+
+        message.channel.send(`Event: [${event.name}] added to group: [${group.name}]`);
+      }
+      else
+      {
+        message.channel.send(`:no_entry_sign: Group: [${values[0]}] doesn\'t exist. :no_entry_sign:`);
+      }
+    }
+    else
+    {
+      message.channel.send(":warning: Please enter the group, event name, date, time and info relating to the event seperated with colons. :warning:");
+    }
+  }
+};
