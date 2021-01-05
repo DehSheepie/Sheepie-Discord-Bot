9c00e7f0de4ce8b20930c32d1bd3d0fb935c2436
Sheepie <chrisparsonsm97@yahoo.co.uk>
2021-01-05 03:30:15 +0000

    created remove event function with basic functionality

--- commands/removeevent.js
+++ commands/removeevent.js
@@ -1,16 +1,37 @@
-const functions = require('../functions.js');
 const fs = require('fs');
+const functions = require('../functions.js');
 
 module.exports = {
   name: 'removeevent',
   description: "Removes a group for events to be added to",
   execute(message, args) {
-
     let text = functions.argsToString(args);
     let values = text.split(':');
+    let groups = functions.getGroupsData();
+
+    // Shows each of the group names
+    // This should get deleted eventually
+    for (i = 0; i < groups.length; i++)
+    {
+      console.log(groups[i]);
+      let group = functions.getGroupData(groups[i].slice(0,-5));
+      console.log(group.name);
+    }
 
     if (values.length === 2)
     {
+      let group = functions.getGroupData(values[0]);
+      let event_index = group.events.indexOf(arg => arg.name == values[1]);
 
+      group.events.splice(event_index, 1);
+
+      fs.writeFileSync(`./commands/data/events/${values[0]}.json`, JSON.stringify(group));
+
+      message.channel.send(`Event: [${values[1]}] has been removed from group: [${values[0]}]`);
     }
+    else
+    {
+      message.channel.send(":warning: Please enter a group followed by the name of the event you wish to remove seperated with a colon. :warning:");
+    }
+  }
 };
